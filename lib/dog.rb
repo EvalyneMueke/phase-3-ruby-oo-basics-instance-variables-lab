5b4b5b7303156b5d5f42183c64045fa19e049e36
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-02-08 08:48:46 +0300

    Completed Instance Variable Code Along

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
+    def name=(dogs_name)
+        @this_dogs_name= dogs_name
+    end
+
+    def name
+        @this_dogs_namegi
+    end
 end
+lassie=Dog.new
+lassie.name ="Lassie"
+
+lassie.name
+
+
+
+odie = Dog.new
+
+odie.name = "Odie"
+
+puts odie.name
